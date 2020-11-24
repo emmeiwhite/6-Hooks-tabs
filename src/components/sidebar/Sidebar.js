a73d4b2de6b51772e2593599ef2ff597a918b2d9
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-24 17:11:38 +0530

    Default active state in sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 
 export default function Sidebar({ tabs, handleContent }) {
-  const [activeTab, setActiveTab] = useState("");
+  const [activeTab, setActiveTab] = useState();
+
   const companies = tabs.map((tab) => ({
     company: tab.company,
     id: tab.id,
   }));
 
+  useEffect(() => {
+    setActiveTab(companies[0].id);
+  }, []);
   // handleBtnClick | Will also manage active state here and use handleContent props as well
   const handleBtnClick = (id) => {
     handleContent(id);
